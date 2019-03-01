7e1601793a70d84d32bf5a1227bf73e87fd3f1cb
liujun <422134235@qq.com>
2019-03-01 18:31:15 +0800

    添加基础工程

---
+++ src/main/java/com/liujun/search/engine/App.java
@@ -0,0 +1,13 @@
+package com.liujun.search.engine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
